80fcb179900f658eccb924f6135b38b788908d45
Ayan <59755186+ayanatherate@users.noreply.github.com>
2023-05-23 11:17:17 +0530

    j7t7ktk

--- perf_regression.py
+++ perf_regression.py
@@ -20,7 +20,7 @@ def make_linear_reg_mods(brand_df,ad_cost,tranf_price,dist_cost_sc,dist_cost_reg
     
     
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
     
     
     

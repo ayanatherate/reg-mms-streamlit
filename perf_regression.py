dd44f3eb2fc5a8204da41c0ce2b01dca45efec50
Ayan <59755186+ayanatherate@users.noreply.github.com>
2023-05-24 16:04:02 +0530

    reberbre

--- perf_regression.py
+++ perf_regression.py
@@ -19,6 +19,7 @@ def make_linear_reg_mods(brand_df,ad_cost,tranf_price,dist_cost_sc,dist_cost_reg
         brand_df[col]=np.log(brand_df[col].values)
         
     st.write(brand_df)
+    brand_df.dropna(inplace=True)
     X=brand_df.drop(['Gross_Sales'],axis=1)
     y=brand_df['Gross_Sales']
 
